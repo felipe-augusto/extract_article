0b4d44fc42f6c87d2ab5d1f2e79c374760f706d0
felipe <fesntmail@gmail.com>
2016-12-02 18:25:32 -0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# extract_article
+
+Small webservice to illustrate how scaleme can deploy docker apps with an auto scalable infrastructure
---
+++ index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var express = require('express');
+var read = require('node-readability');
+
+// Constants
+var PORT = 8080;
+
+// App
+var app = express();
+
+app.get('/', function (req, res) {
+	if(req.query.url) {
+		read(req.query.url, function(err, article, meta) {
+			if(err) {
+				res.json({"error": err});
+			} else {
+				res.json({"html" : article.content, "title": article.title});
+			}
+			article.close();
+		})
+	} else {
+		res.json({"error" : "Nenhuma url foi recebida"});
+	}
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
